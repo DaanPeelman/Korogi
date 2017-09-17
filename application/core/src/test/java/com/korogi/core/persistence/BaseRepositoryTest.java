84a50580e9bb6a88b99cec2c7d7b033f307b2a60
Daan Peelman <daanpeelman@gmail.com>
2017-09-17 12:43:34 +0200

    Rename test persistence config && some reformat

--- application/core/src/test/java/com/korogi/core/persistence/BaseRepositoryTest.java
+++ application/core/src/test/java/com/korogi/core/persistence/BaseRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.korogi.core.persistence;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -8,18 +10,20 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 /**
  * @author Daan Peelman
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring/springApplicationContext-test-persistence.xml" })
+@ContextConfiguration(locations = { "classpath:spring/test-persistence-config.xml" })
 @Transactional
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+@TestExecutionListeners(
+    {
+        DependencyInjectionTestExecutionListener.class,
         DirtiesContextTestExecutionListener.class,
-        TransactionDbUnitTestExecutionListener.class })
+        TransactionDbUnitTestExecutionListener.class
+    }
+)
 public abstract class BaseRepositoryTest {
     @PersistenceContext
     protected EntityManager em;

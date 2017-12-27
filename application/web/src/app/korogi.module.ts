5c1b5c05cee035056d3c55a5c300160ee0f51539
Daan Peelman <daanpeelman@gmail.com>
2017-12-27 17:38:07 +0100

    Some basic styling for header

--- application/web/src/app/korogi.module.ts
+++ application/web/src/app/korogi.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { KorogiComponent } from "./korogi.component";
+import { HeaderModule } from "./shared/header/header.module";
 
 @NgModule({
   declarations: [
     KorogiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [KorogiComponent]

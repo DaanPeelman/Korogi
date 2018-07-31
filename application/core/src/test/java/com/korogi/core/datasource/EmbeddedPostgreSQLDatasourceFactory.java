d37257891e74c0a503f351b2c5358d0281c1ecab
Daan Peelman <daanpeelman@gmail.com>
2018-07-31 18:17:44 +0200

    Embedded postgreSQL db for tests

---
+++ application/core/src/test/java/com/korogi/core/datasource/EmbeddedPostgreSQLDatasourceFactory.java
@@ -0,0 +1,23 @@
+package com.korogi.core.datasource;
+
+import java.io.IOException;
+import javax.annotation.PreDestroy;
+import javax.sql.DataSource;
+import com.opentable.db.postgres.embedded.EmbeddedPostgres;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmbeddedPostgreSQLDatasourceFactory {
+    private EmbeddedPostgres embeddedPostgres;
+
+    public DataSource getDatasource() throws IOException {
+        embeddedPostgres = EmbeddedPostgres.start();
+
+        return embeddedPostgres.getPostgresDatabase();
+    }
+
+    @PreDestroy
+    public void onDestroy() throws IOException {
+        embeddedPostgres.close();
+    }
+}

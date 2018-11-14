3f0944342c5f4692190f9168f04201a4db22c531
Daan Peelman <daanpeelman@gmail.com>
2018-11-14 19:53:25 +0100

    Front-end with bootstrap (still needs cleaning)

--- application/web/src/app/shared/header/header.component.ts
+++ application/web/src/app/shared/header/header.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  transparant: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.transparant = true;
   }
 
 }

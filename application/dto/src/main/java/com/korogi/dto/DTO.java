dd079d2d131e56938c195f36eac173de48fbd96e
Daan Peelman <daanpeelman@gmail.com>
2020-02-16 00:24:20 +0100

    Move interface DTORelation into DTO interface

--- application/dto/src/main/java/com/korogi/dto/DTO.java
+++ application/dto/src/main/java/com/korogi/dto/DTO.java
@@ -1,10 +1,16 @@
 package com.korogi.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.korogi.dto.annotation.ExcludeFromTypescriptGenerator;
 
 public interface DTO {
     @JsonProperty(value = "type")
     @ExcludeFromTypescriptGenerator
     String type();
+
+    interface DTORelation {
+        @JsonValue
+        String getValue();
+    }
 }

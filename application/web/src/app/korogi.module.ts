6c68a07a5804b256998bbc31c2a3969e7b86cd66
Daan Peelman <daanpeelman@gmail.com>
2020-09-16 19:16:41 +0200

    Refactor docker build. Now with 2 docker images

--- application/web/src/app/korogi.module.ts
+++ application/web/src/app/korogi.module.ts
@@ -1,24 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { KorogiComponent } from "./korogi.component";
 import { HeaderModule } from "./shared/components/header/header.module";
-import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { AnimeService } from "./shared/services/anime/anime.service";
 import { RouterModule } from "@angular/router";
 import { AnimeModule } from "./anime/anime.module";
 import { RelationLoaderService } from "./shared/services/relation-loader/relation-loader.service";
 import { ModelMapperService } from "./shared/services/model-mapper/model-mapper.service";
 import { MapperModule } from "./shared/mapper/mapper.module";
-import { KeycloakAngularModule, KeycloakService } from "keycloak-angular";
 
 @NgModule({
     declarations: [
         KorogiComponent
     ],
     imports: [
-        KeycloakAngularModule,
-
         BrowserModule,
         HeaderModule,
         HttpClientModule,
@@ -29,13 +26,6 @@ import { KeycloakAngularModule, KeycloakService } from "keycloak-angular";
         AnimeModule
     ],
     providers: [
-        {
-            provide: APP_INITIALIZER,
-            useFactory: initializer,
-            multi: true,
-            deps: [KeycloakService, HttpClient]
-        },
-
         RelationLoaderService,
         ModelMapperService,
         AnimeService
@@ -43,8 +33,4 @@ import { KeycloakAngularModule, KeycloakService } from "keycloak-angular";
     bootstrap: [KorogiComponent]
 })
 export class KorogiModule {
-}
-
-export function initializer(keycloak: KeycloakService, httpClient: HttpClient): () => Promise<any> {
-    return (): Promise<any> => keycloak.init({config: "rest/config/keycloak"});
 }

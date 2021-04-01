9a175ce4792b351cda0438b7096c0ca7bc24dbdd
Daan Peelman <daanpeelman@gmail.com>
2021-04-01 19:44:40 +0200

    Springboot-oAuth2 - make tests run for the moment...

--- application/web/src/app/login/login-callback/login-callback.component.spec.ts
+++ application/web/src/app/login/login-callback/login-callback.component.spec.ts
@@ -1,25 +1,57 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, inject, TestBed } from "@angular/core/testing";
 
-import { LoginCallbackComponent } from './login-callback.component';
+import { LoginCallbackComponent } from "./login-callback.component";
+import { ActivatedRoute } from "@angular/router";
+import { StubUtil } from "../../testing/util/stub-util";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AuthenticationService } from "../../shared/services/authentication/authentication-service";
+import { instance, mock } from "ts-mockito";
+import { UserService } from "../../shared/services/user/user.service";
 
-describe('LoginCallbackComponent', () => {
-  let component: LoginCallbackComponent;
-  let fixture: ComponentFixture<LoginCallbackComponent>;
+describe("LoginCallbackComponent", () => {
+    let component: LoginCallbackComponent;
+    let fixture: ComponentFixture<LoginCallbackComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginCallbackComponent ]
-    })
-    .compileComponents();
-  });
+    let activatedRouteStub;
+    const authenticationServiceMock: AuthenticationService = mock(AuthenticationService);
+    const userServiceMock: UserService = mock(UserService);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginCallbackComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule(
+            {
+                declarations: [LoginCallbackComponent],
+                imports: [
+                    RouterTestingModule
+                ],
+                providers: [
+                    {
+                        provide: ActivatedRoute,
+                        useFactory: () => StubUtil.subActivatedRoute()
+                    },
+                    {
+                        provide: AuthenticationService,
+                        useFactory: () => instance(authenticationServiceMock)
+                    },
+                    {
+                        provide: UserService,
+                        useFactory: () => instance(userServiceMock)
+                    }
+                ]
+            }
+        ).compileComponents();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(inject([ActivatedRoute], (_activatedRouteStub: ActivatedRoute) => {
+        activatedRouteStub = _activatedRouteStub;
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoginCallbackComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
